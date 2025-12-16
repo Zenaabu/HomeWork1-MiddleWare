bcff7d677922de0522ce45f7024da2e4a12112e9
Zenaabu <zenaabu11@gmail.com>
2025-12-16 13:09:16 +0200

    homework1
    added comments to the homework

--- app.js
+++ app.js
@@ -13,23 +13,31 @@ const users = [
   { username: "user123", password: "user123", role: "user" },
   { username: "user678", password: "user678", role: "user" },
 ];
+
+// a variables for adding the hello
 let h1 = "";
 let h2 = "";
 
+// middleware for all paths that adds Hello1 to the string h1
 app.use((req, res, next) => {
   h1 += " Hello1";
   next();
 });
 
+// middleware for all paths that adds Hello2 to the string h1
 app.use((req, res, next) => {
   h2 += " Hello2";
   next();
 });
 
+// path 1 for "/users"
+// when we send the string to the browser we add to it the strings h1 and h2
 app.get("/users", (req, res) => {
   res.status(200).send("<h1>Users Page</h1> <p>Welcome again!</p>" + h1 + h2);
 });
 
+// path 2 for "/kuku"
+// when we send the string to the browser we add to it the strings h1 and h2
 app.get("/kuku", (req, res) => {
   res.status(200).send("<h1>Kuku Page</h1> <p>Welcome again!</p>" + h1 + h2);
 });
